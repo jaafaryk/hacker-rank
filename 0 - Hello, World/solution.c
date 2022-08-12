0c93d9fef3baa8f555dbf18c1d942455615d02c4
Khalid Jaafary <Kjaafary@student.42istanbul.com.tr>
2022-08-12 23:09:28 +0300

    how scanf works

--- 0 - Hello, World/solution.c
+++ 0 - Hello, World/solution.c
@@ -4,6 +4,12 @@ int main(void)
 {
 	char s[100];
 
+    /* - %*[^\n] scans everything until a newline \n
+         (Doesn't scan \n) and discards it,i.e, doesn't
+         store the scanned data anywhere.
+       - %*c scans and discards the \n character, upto
+         which the first format specifier has scanned.
+    */
     scanf("%[^\n]%*c", &s);
     printf("Hello, World!\n");
     printf("%s", s); 
